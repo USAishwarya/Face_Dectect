13e387f67443bb4e193f6ce046e84c61cd03c677
USAishwarya <aishwarya7092004@gmail.com>
2025-04-20 13:43:57 +0530

    logger removed

--- face_detection.py
+++ face_detection.py
@@ -6,7 +6,7 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 # Load an image
 image = cv2.imread('myImg.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-print("image gray scale done")
+
 # Detect faces
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
